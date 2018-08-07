54bd85e3e1dff062999d76fbe0b560c8fafed475
Sam Rechsteiner <samrech@umich.edu>
2018-08-07 13:00:27 -0400

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,12 @@ class UsersController < ApplicationController
 	before_action :admin_user,	   only: :destroy
 
 	def index
-		@users = User.paginate(page: params[:page])
+		@users = User.where(activated: true).paginate(page: params[:page])
 	end
 
 	def show
 		@user = User.find(params[:id])
+		redirect_to root_url and return unless @user.activated?
 	end
 
 	def new
@@ -18,9 +19,9 @@ class UsersController < ApplicationController
 	def create 
 		@user = User.new(user_params)
 		if @user.save
-			log_in(@user)
-			flash[:success] = "Welcome to the Sample App!"
-			redirect_to @user
+			@user.send_activation_email
+			flash[:info] = "Please check your email to activate your account."
+			redirect_to root_url
 		else
 			render 'new'
 		end
@@ -46,10 +47,6 @@ class UsersController < ApplicationController
 		redirect_to users_url
 	end
 
-	private def user_params
-		params.require(:user).permit(:name, :email, :password, :password_confirmation)
-	end
-
 	def logged_in_user
 		unless logged_in?
 			store_location
@@ -65,5 +62,9 @@ class UsersController < ApplicationController
 
 	def admin_user
 		redirect_to(root_url) unless current_user.admin?
+	end
+
+	private def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+	
+	def setup
+		ActionMailer::Base.deliveries.clear
+	end
+
 	test "Invalid user should not be added" do
 		get signup_path
 		assert_no_difference 'User.count' do
@@ -20,9 +25,24 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 									  password: "password",
 									  password_confirmation: "password" } }
 		end
+		assert_equal 1, ActionMailer::Base.deliveries.size
+		
+		user = assigns(:user)
+		assert_not user.activated?
+		
+		log_in_as(user)
+		assert_not is_logged_in?
+
+		get edit_account_activation_path("invalid token", email: user.email)
+		assert_not is_logged_in?
+
+		get edit_account_activation_path(user.activation_token, email: 'wrong')
+		assert_not is_logged_in?
+
+		get edit_account_activation_path(user.activation_token, email: user.email)
+		assert user.reload.activated?
 		follow_redirect!
 		assert_template 'users/show'
-		assert_not flash.empty?
 		assert is_logged_in?
 	end
 end
